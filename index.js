c0d92661cf5226aada6e7762eaa5e83accc1ecf9
mcumm64 <mcumm64@gmail.com>
2018-10-21 21:05:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays() {
   return "Happy holidays!";
 }
-function happyHolidaysTo (event, name) {
+function happyHolidaysTo (name) {
   return "Happy holidays, " + name + "!";
 }
 function happyHolidayTo(event, name) {
